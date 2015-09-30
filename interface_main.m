6333f767b91c377ccbc24dd4a48a9aba7e2297bd
Casper CY Chiang <cychiang0823@gmail.com>
2015-09-30 08:38:06 +0200

    update for user interface

--- interface_main.m
+++ interface_main.m
@@ -1,2 +1,21 @@
 % interface of image_enhancement.m
 
+function [] = interface_main()
+    ui = figure('Visible','off','Position',[360,500,450,285]);
+    % Push Button   : Load Image.
+    btn = uicontrol('Style', 'pushbutton', 'String', 'Clear', ...
+        'Position', [20 20 50 20], ...
+        'Callback', 'cla');
+    % Slider Bar    : Adjust variable.
+    sld = uicontrol('Style', 'slider',...
+        'Min',1,'Max',50,'Value',41,...
+        'Position', [400 20 120 20],...
+        'Callback', @k0_value); 
+    % Push Button   : Image Enhancement.
+    % Processing    : Run and Show Result.
+    ui.Visible = 'on';
+    
+    function k0_value(source, callbackdata)
+        var = source.Value;
+    end
+end
